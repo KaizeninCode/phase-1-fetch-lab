5f94e5d35e8345145f291996d220795541aceb6e
Phil Wekullo <philwekullo@gmail.com>
2023-12-27 02:50:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+
+  //return the fetch promise from the fetchBooks function and use the then method to handle the promise
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  //if an error occurs while fetching the books, we log the error to the console and return an empty array to prevent further execution
+  .catch(error => {
+    console.error('Error fetching books:', error)
+    return []
+  })
   
 }
 
@@ -13,5 +22,7 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+  fetchBooks().then(books => {
+    renderBooks(books)
+  });
 });
